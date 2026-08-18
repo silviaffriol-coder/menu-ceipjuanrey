c06634c1f505f26aa59de04ef83392c1055f9431
silviaffriol-coder <silviaffriol@gmail.com>
2026-08-18 16:41:08 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,27 @@
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
 
