a8381a0421767400b958104ddcf6656455c95413
silviaffriol-coder <silviaffriol@gmail.com>
2026-09-09 19:08:02 +0200

    Update menus.js

--- menus.js
+++ menus.js
@@ -1403,3 +1403,12 @@ const MENUS_SEN_MARISCO = {
     "2027-06-21": { primeiro:"", segundo:"", sobremesa:"" }
 
 }; // FIN DO MENUS_SEN_MARISCO COMPLETO
+   // Último día lectivo
+    "2027-06-21": { primeiro:"", segundo:"", sobremesa:"" }
+export {
+  MENUS_BASAL,
+  MENUS_SEN_LACTOSA,
+  MENUS_SEN_GLUTE,
+  MENUS_MUSULMAN,
+  MENUS_SEN_MARISCO
+};
